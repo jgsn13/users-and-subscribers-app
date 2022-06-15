337972dbc302a190e80f45b85c95d2fd930b58f5
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-06-15 19:05:36 -0300

    update: setup bcryptjs

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,5 +1,6 @@
 import { IsEmail, Length } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import bcrypt from "bcryptjs";
 
 @Entity("users")
 class User {
@@ -17,6 +18,12 @@ class User {
   @Column()
   @Length(3)
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
 
 export default User;
