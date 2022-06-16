836ec39a321af578d25c83e76e7867dbed62884e
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-06-15 21:39:00 -0300

    fix: move all code from app.ts to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,19 @@
-import app from "./app";
+import "reflect-metadata";
+import express from "express";
+
+import "./database/connection";
+
+import routes from "./routes"
+import requestLogger from "./middlewares/requestLogger";
+
+const app = express();
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(routes)
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log("Server started!");
+  console.log(`🚀 Server started at port ${port}!`);
 });
