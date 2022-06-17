693082f1d1e8b17a239957751ac532d4fa01aaac
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-06-17 10:07:37 -0300

    update: add history for react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import GlobalStyle from "./styles/GlobalStyles";
 import Header from "./components/Header";
 
-import { BrowserRouter } from "react-router-dom";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import Routing from "./routes";
+
+import history from "./services/history";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HistoryRouter history={history}>
       <Header />
       <Routing />
       <GlobalStyle />
-    </BrowserRouter>
+    </HistoryRouter>
   );
 }
 
